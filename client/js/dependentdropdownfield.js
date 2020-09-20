9c3fba8d8756e70b25bdf3448466450f4e38c234
Sergey Shevchenko <gurucomkz@users.noreply.github.com>
2020-09-20 18:16:59 +1200

    Update dependentdropdownfield.js
    feat: add compatibility with other inputs that are lazy-loaded, mark option selected when present in json

--- client/js/dependentdropdownfield.js
+++ client/js/dependentdropdownfield.js
@@ -1,39 +1,49 @@
 jQuery.entwine("dependentdropdown", function ($) {
 
+
 	$(":input.dependent-dropdown").entwine({
 		onmatch: function () {
 			var drop = this;
-			var depends = ($(":input[name=" + drop.data('depends').replace(/[#;&,.+*~':"!^$[\]()=>|\/]/g, "\\$&") + "]"));
 
-			this.parents('.field:first').addClass('dropdown');
+            this.parents('.field:first').addClass('dropdown');
+            var checkInit = function() {
+                var depends = ($(":input[name=" + drop.data('depends').replace(/[#;&,.+*~':"!^$[\]()=>|\/]/g, "\\$&") + "]"));
+                if(!depends.length) {
+                    return window.setTimeout(checkInit,300);
+                }
+                var query = function () {
+                    var value = this.value || this.val();
+                    if (!value) {
+                        drop.disable(drop.data('unselected'));
+                    } else {
+                        drop.disable("Loading...");
 
-			depends.change(function () {
-				if (!this.value) {
-					drop.disable(drop.data('unselected'));
-				} else {
-					drop.disable("Loading...");
+                        $.get(drop.data('link'), {
+                            val: value
+                        },
+                        function (data) {
+                            drop.enable();
 
-					$.get(drop.data('link'), {
-						val: this.value
-					},
-					function (data) {
-						drop.enable();
+                            if (drop.data('empty') || drop.data('empty') === "") {
+                                drop.append($("<option />").val("").text(drop.data('empty')));
+                            }
 
-						if (drop.data('empty') || drop.data('empty') === "") {
-							drop.append($("<option />").val("").text(drop.data('empty')));
-						}
+                            $.each(data, function () {
+                                drop.append($("<option />").val(this.k).text(this.v).prop('selected',!!this.selected));
+                            });
+                            drop.trigger("liszt:updated").trigger("chosen:updated").trigger("change");
+                        });
+                    }
+                };
+                query.call(depends);
+                depends.change(query);
 
-						$.each(data, function () {
-							drop.append($("<option />").val(this.k).text(this.v));
-						});
-						drop.trigger("liszt:updated").trigger("chosen:updated").trigger("change");
-					});
-				}
-			});
-
-			if (!depends.val()) {
-				drop.disable(drop.data('unselected'));
-			}
+                if (!depends.val()) {
+                    drop.disable(drop.data('unselected'));
+                }
+            }
+            drop.disable(drop.data('unselected'));
+            checkInit();
 		},
 		disable: function (text) {
 			this.empty().append($("<option />").val("").text(text)).attr("disabled", "disabled").trigger("liszt:updated").trigger("chosen:updated");
